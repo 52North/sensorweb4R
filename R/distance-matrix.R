01eec83bbc0a251f7c0252efbf669ff9ed95b0bc
Christian Autermann <c.autermann@52north.org>
2015-01-29 15:59:18 +0100

    updated distance matrix methods to new classes

--- R/distance-matrix.R
+++ R/distance-matrix.R
@@ -12,56 +12,140 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#' Distance Matrix Creation
-#'
-#' Creates a distance matrix for the supplied coordinates.
-#'
-#' @param x a Nx2 matrix of WGS84 coordinates or a \code{sp::SpatialPointsDataFrame}
-#' @return the distance matrix of class \code{dist}
-#' @export
-#' @examples
-#' \dontrun{
-#' stations <- get_stations(url)
-#' dm <- create_distance_matrix(stations)
-#' }
-create_distance_matrix <- function(x) {
-    n <- dim(x)[1]
-    m <- matrix(0, ncol=n, nrow=n)
-    for (i in 1:n) {
-        for (j in i:n) {
-            m[i,j] <- m[j,i] <- geosphere::distVincentyEllipsoid(x[i,], x[j,])
-        }
-    }
-    return(as.dist(m));
-}
 
-#' Distance Matrix Deserialization
-#'
-#' Saves a distance matrix to disk.
-#'
+#' @title
+#' Distance matrix methods
+#' @description
+#' Methods to calculate distances between \linkS4class{Station}s
+#' and to find the nearest \code{Station}s.
+#' @rdname distance-matrix-methods
+#' @name distance-matrix-methods
+NULL
+
+
+#' @description
+#' \code{saveDistanceMatrix} saves a distance matrix to disk.
 #' @param dm the distance matrix
 #' @param file the file name
 #' @export
 #' @examples
 #' \dontrun{
-#' save_distance_matrix(dm, 'distance_matrix.rds')
+#' saveDistanceMatrix(dm, 'distance_matrix.rds')
 #' }
-#' @describeIn create_distance_matrix
-save_distance_matrix <- function(dm, file) {
+#' @rdname distance-matrix-methods
+saveDistanceMatrix <- function(dm, file) {
     saveRDS(dm, file, compress=TRUE)
 }
 
-#' Distance Matrix Serialization
-#'
-#' Reads the a distance matrix from disk.
-#'
+
+#' @description
+#' \code{readDistanceMatrix} reads the a distance matrix from disk.
 #' @param file the file name
 #' @export
 #' @examples
 #' \dontrun{
-#' dm <- read_distance_matrix('distance_matrix.rds')
+#' dm <- readDistanceMatrix('distance_matrix.rds')
 #' }
-#' @describeIn create_distance_matrix
-read_distance_matrix <- function(file) {
+#' @rdname distance-matrix-methods
+readDistanceMatrix <- function(file) {
     return(readRDS(file))
 }
+
+#' @description
+#' \code{nearestStations} gets the \code{n} stations nearest
+#' to \code{station}.
+#' @param x the object to find the nearest stations for
+#' @return a subset of \code{stations} orderd by their distance
+#'          to \code{x}, enriched with an additional
+#'          \code{distance} attribute
+#' @export
+#' @rdname distance-matrix-methods
+setGeneric("nearestStations", function(x, ...) standardGeneric("nearestStations"))
+
+#' Distance Matrix Creation
+#'
+#' \code{distanceMatrix} creates a distance matrix for the supplied object.
+#'
+#' @param x The object to create a distance matrix for.
+#' @return the distance matrix of class \code{dist}
+#' @export
+#' @rdname distance-matrix-methods
+setGeneric("distanceMatrix", function(x, ...) standardGeneric("distanceMatrix"))
+
+#' @export
+#' @rdname distance-matrix-methods
+setGeneric("distance", function(x) standardGeneric("distance"))
+
+setClass("DistanceStation",
+         contains = "Station",
+         slots = c(distance = "numeric"),)
+
+DistanceStation <- function(station, distance) {
+    station <- as.Station(station)
+    new("DistanceStation", id = id(station), label = label(station),
+        geometry = geometry(station), endpoint = endpoint(station),
+        distance = stretch(length(station), distance, NA, as.numeric))
+}
+
+
+#' @param stations the \linkS4class{Station}s for the distance
+#'        calculation (requested if \code{NULL})
+#' @param dm the distance matrix for the stations (generated
+#'        if \code{NULL})
+#' @param n the number of stations that should be returned
+#' @rdname distance-matrix-methods
+setMethod("nearestStations",
+          signature(x = "Station"),
+          function(x, stations = NULL, dm = NULL, n = 1, ...) {
+
+              if (is.null(stations)) {
+                  stations <- stations(endpoint(x))
+              }
+
+              if (is.null(dm)) {
+                  dm <- distanceMatrix(stations)
+              }
+              if (n <= 0) {
+                  warning('n <= 0. Setting n to 1')
+                  n <- 1
+              }
+              if (n > length(stations) - 1) {
+                  warning('n greater than number of stations. Setting to length(stations)-1')
+                  n <- length(stations) - 1
+              }
+
+              dm <- as.matrix(dm)
+              # the index of the station
+              idx <- match(id(x), id(stations))
+              # the top n nearest indices
+              top <- as.integer(names(sort(dm[idx,])[seq(2, n + 1)]))
+
+              DistanceStation(stations[top], dm[idx,top])
+          })
+
+
+
+#' @rdname distance-matrix-methods
+setMethod("distance",
+          signature(x = "DistanceStation"),
+          function(x) x@distance)
+
+#' @rdname distance-matrix-methods
+setMethod("distanceMatrix",
+          signature(x = "SpatialPoints"),
+          function(x, ...) {
+              n <- length(x)
+              dm <- matrix(0, ncol=n, nrow=n)
+              for (i in 1:n) {
+                  for (j in i:n) {
+                      dm[i,j] <- dm[j,i] <-
+                          geosphere::distVincentyEllipsoid(x[i,], x[j,])
+                  }
+              }
+              return(as.dist(dm));
+          })
+
+#' @rdname distance-matrix-methods
+setMethod("distanceMatrix",
+          signature(x = "Station"),
+          function(x, ...) distanceMatrix(geometry(x)))
